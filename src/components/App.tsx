f463a58a912cbdd049260df7570ee23fc8e42838
phau <1057021+ph4un00b@users.noreply.github.com>
2022-05-21 16:48:21 -0500

    chore(Root): add react router v6

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,21 @@
-import React from 'https://esm.sh/react@18'
+import React from "https://esm.sh/react@18";
 import { useState } from "https://esm.sh/react@18";
+import { Link } from "https://esm.sh/react-router-dom@6";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>Deno + React 18!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-      </header>
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'https://esm.sh/react@18'
-import ReactDOM from 'https://esm.sh/react-dom@18/client'
+import React from "https://esm.sh/react@18";
+import ReactDOM from "https://esm.sh/react-dom@18/client";
 import App from "./components/App.tsx";
+import { BrowserRouter } from "https://esm.sh/react-router-dom@6";
 
 /* document was configured with proper deno.json */
 /* https://deno.land/manual/typescript/configuration#using-the-lib-property */
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
 ReactDOM.createRoot(root!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
