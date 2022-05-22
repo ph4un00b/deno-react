5c956d9d91b77f9e91d05e2bc14c7399c3a40db6
phau <1057021+ph4un00b@users.noreply.github.com>
2022-05-21 22:26:53 -0500

    feat(Root): add search parameter on input changes

--- src/routes/invoices.tsx
+++ src/routes/invoices.tsx
@@ -1,9 +1,14 @@
 import React from "https://esm.sh/react@18";
-import { NavLink, Outlet } from "https://esm.sh/react-router-dom@6";
+import {
+  NavLink,
+  Outlet,
+  useSearchParams,
+} from "https://esm.sh/react-router-dom@6";
 import { getInvoices } from "../data.ts";
 
 export default function Invoices() {
   const invoices = getInvoices();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <div style={{ display: "flex" }}>
@@ -13,22 +18,43 @@ export default function Invoices() {
           padding: "1rem",
         }}
       >
-        {invoices.map((invoice) => (
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: "block",
-                margin: "1rem 0",
-                backgroundColor: isActive ? "yellowgreen" : "",
-                color: isActive ? "brown" : "",
-              };
-            }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={(event) => {
+            const filter = event.target.value;
+
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+
+        {invoices
+          .filter((invoice) => {
+            const filter = searchParams.get("filter");
+            if (!filter) return true;
+
+            const name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              style={({ isActive }) => {
+                return {
+                  display: "block",
+                  margin: "1rem 0",
+                  backgroundColor: isActive ? "yellowgreen" : "",
+                  color: isActive ? "brown" : "",
+                };
+              }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
 
       <Outlet />
