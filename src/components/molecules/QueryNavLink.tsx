84fbe92eaf3eb5ff3b7a92522b57edc799fb03ff
phau <1057021+ph4un00b@users.noreply.github.com>
2022-05-21 23:07:17 -0500

    chore(Root): add active query link molecule component

--- src/components/molecules/QueryNavLink.tsx
+++ src/components/molecules/QueryNavLink.tsx
@@ -1,11 +1,34 @@
 import React from "https://esm.sh/react@18";
 import {
+  Link,
+  LinkProps,
   NavLink,
   NavLinkProps,
   useLocation,
+  useSearchParams,
 } from "https://esm.sh/react-router-dom@6";
 
 export function QueryNavLink({ to, ...props }: NavLinkProps) {
   const location = useLocation();
   return <NavLink to={to + location.search} {...props} />;
+}
+
+type ActiveProps = LinkProps & {
+  query: string;
+  queryParams: string;
+};
+
+export function ActiveQueryLink(spec: ActiveProps) {
+  const { query, queryParams, to, ...props } = spec;
+  const [params] = useSearchParams();
+  const location = useLocation();
+  const isActive = params.getAll(query).includes(queryParams);
+
+  return (
+    <Link
+      style={{ color: isActive ? "red" : "" }}
+      to={`/${location.pathname}?${query}=${queryParams}`}
+      {...props}
+    />
+  );
 }
